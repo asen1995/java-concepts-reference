392d63f7b0ba655408605920581642f4a7054829
Asen Nikolaev <asen.nikolaev@nevexis.com>
2021-01-18 15:34:26 +0200

    more example streams

--- src/main/java/com/java/streams/api/Streams.java
+++ src/main/java/com/java/streams/api/Streams.java
@@ -65,18 +65,35 @@ public class Streams {
 		System.out.println("concatenate Asen to every String");
 		myList.stream().map(s -> s.concat("asen")).forEach(System.out::println);
 
-		List<Book> books = Arrays.asList(new Book("kniga1", 2014, "sd"),new Book("kniga2", 2015, "sd"),new Book("kniga2", 2066, "sd"));
-		
+		List<Book> books = Arrays.asList(new Book("kniga1", 2014, "sd"), new Book("kniga2", 2015, "sd"),
+				new Book("kniga2", 2066, "sd"));
+
 		System.out.println();
 		System.out.println("List to Map");
 		Map<Integer, String> booksmap = books.stream().collect(Collectors.toMap(Book::getReleaseYear, Book::getName));
 		System.out.println(booksmap);
-		
+
 		System.out.println();
 		System.out.println("to Set");
 		Set<Book> setbooks = books.stream().collect(Collectors.toSet());
 		System.out.println(setbooks);
-		
+
+		System.out.println();
+		System.out.println("find atleast one");
+		Stream<String> sampleStream = Stream.of("d2", "a2", "b1", "b3", "c");
+
+		String result = sampleStream.filter(string -> string.equalsIgnoreCase("b14")).findAny().orElse(null);
+		System.out.println(result);
+
+		List<String> anotherList = Arrays.asList("a1", "a2", "b1", "c2", "c1", "a1", "a2");
+
+		System.out.println();
+		System.out.println("create another list with specific strings - contains 1");
+
+		List<String> anotherList2 = anotherList.stream().filter(string -> string.contains("1")).sorted()
+				.collect(Collectors.toList());
+		System.out.println(anotherList2);
+
 		return myList.stream();
 	}
 
